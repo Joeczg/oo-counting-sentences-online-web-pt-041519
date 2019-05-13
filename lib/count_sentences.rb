583b5290080e3eb2bea500f6087f1c159679cf73
zhenggao cai <caizhengao1992@gmail.com>
2019-05-13 04:03:58 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -18,7 +18,16 @@ self.end_with?("!")
     new_array = []
   if self.size == 0 
     return 0 
+  else
     new_array = self.split(/\?|\.|!/)
+    n = 0 
+    count = 0 
+    for n in 0..new_array.size - 1 
+     if new_array[n].size == 0 
+       count += 1 
+     end
+      return new_array.size - count
+    end
     
   end
 end
